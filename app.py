f5f32fc984573f84eb1a3936a01790703cf5fca4
xim0608 <xim.ryuki@gmail.com>
2017-01-16 23:21:48 +0900

    second

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/howto')
 def how_to():
     return render_template('howto.html')
+
+
+@app.route('/test/<name>')
+def test(name):
+    return 'hello {}'.format(name)
 
 
 if __name__ == '__main__':
