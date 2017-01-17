ff0680c8bdab4320b7e735aac54139fdca4ae6ef
Ryuki <xim.ryuki@gmail.com>
2017-01-18 01:10:42 +0900

    kairyousimasita

--- flaskr/models.py
+++ flaskr/models.py
@@ -6,9 +6,10 @@ from flaskr import db
 
 
 class User(db.Model):
-    # columns
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
+    catname = db.Column(db.String(64), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     _password = db.Column('password', db.String(128), nullable=False)
 
@@ -23,7 +24,6 @@ class User(db.Model):
     password_descriptor = property(_get_password, _set_password)
     password = synonym('_password', descriptor=password_descriptor)
 
-
     def check_password(self, password):
         password = password.strip()
         if not password:
@@ -32,24 +32,25 @@ class User(db.Model):
 
     @classmethod
     def authenticate(cls, query, email, password):
-        user = query(cls).filter(cls.email==email).first()
+        user = query(cls).filter(cls.email == email).first()
         if user is None:
             return None, False
         return user, user.check_password(password)
 
     def __repr__(self):
         return '<User id={self.id} email={self.email!r}>'.format(self=self)
-    # # @hybrid_property
-    # def password(self):
-    #     return self._password
-    #
-    #
-    # @password.setter
-    # def _set_password(self, plaintext):
-    #     self._password = bcrypt.generate_password_hash(plaintext)
-    #
-    # def is_collect_password(self, plaintext):
-    #     return bcrypt.check_password_hash(self._password, plaintext)
+
+
+class CatAction(db.Model):
+    __tablename__= 'actions'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    time = db.Column(db.String, nullable=False)
+
+
+
+class Waiting(db.Model):
+    __tablename__= 'waitings'
+    id = db.Column(db.Integer(), primary_key=True)
 
 
 def init():
--- flaskr/views.py
+++ flaskr/views.py
@@ -31,7 +31,8 @@ def user_edit(user_id):
 @app.route('/users/create/', methods=['GET', 'POST'])
 def user_create():
     if request.method == 'POST':
-        user = User(name=request.form['name'], email=request.form['email'], password=request.form['password'])
+        user = User(name=request.form['name'], catname=request.form['catname'],
+                    email=request.form['email'], password=request.form['password'])
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('user_list'))
@@ -44,9 +45,14 @@ def user_delete(user_id):
         response = jsonify({'status': 'Not Found'})
         response.status_code = 404
         return response
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'status': 'OK'})
+    elif session['user_id'] != user_id:
+        response = jsonify({'status': 'You do not have permissions'})
+        response.status_code = 404
+        return response
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'status': 'OK'})
 
 @app.route('/login', methods=["GET","POST"])
 def login():
@@ -55,7 +61,7 @@ def login():
         if authenticated:
             session['user_id'] = user.id
             flash('You were logged in')
-            return redirect(url_for('show_feed'))
+            return redirect(url_for('user_list'))
         else:
             flash('Invalid email or password')
     return render_template('login.html')
@@ -64,7 +70,7 @@ def login():
 def logout():
     session.pop('user_id', None)
     flash('You were logged out')
-    return redirect(url_for('show_feed'))
+    return redirect(url_for('home'))
 
 # @app.route('/login', methods=["GET", "POST"])
 # def login():
